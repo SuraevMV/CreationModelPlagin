eaf945e938967cb41deb01a1d8ae40e419810dcc
SuraevMV <96659813+SuraevMV@users.noreply.github.com>
2022-12-30 00:17:17 +0300

    Добавьте файлы проекта.

---
+++ CreationModel.cs
@@ -0,0 +1,72 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlagin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CreationModel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            Level level1;
+            Level level2;
+            FirstLevels(doc, out level1, out level2);
+
+            int width = 18000;
+            int depth = 12000;
+            CreateWalls(doc, level1, level2, width, depth);
+            
+            return Result.Succeeded;
+        }
+        private static void CreateWalls(Document doc, Level level1, Level level2, int width, int depth)
+        {
+
+            double widthWall = UnitUtils.ConvertToInternalUnits(width, UnitTypeId.Millimeters);
+            double depthWall = UnitUtils.ConvertToInternalUnits(depth, UnitTypeId.Millimeters);
+            double dx = widthWall / 2;
+            double dy = depthWall / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dx, -dy, 0));
+            points.Add(new XYZ(dx, -dy, 0));
+            points.Add(new XYZ(dx, dy, 0));
+            points.Add(new XYZ(-dx, dy, 0));
+            points.Add(new XYZ(-dx, -dy, 0));
+
+            List<Wall> walls = new List<Wall>();
+
+            Transaction transaction = new Transaction(doc, "Построение стен");
+            transaction.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                walls.Add(wall);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+            }
+            transaction.Commit();
+        }
+        private static void FirstLevels(Document doc, out Level level1, out Level level2)
+        {
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            level1 = listLevel
+                .Where(x => x.Name.Equals("Уровень 1"))
+                .FirstOrDefault();
+            level2 = listLevel
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+        }
+    }
+}
